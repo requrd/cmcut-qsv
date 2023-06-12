b1bae35782867df9239ed4140e381115e20ceef2
Yuma <requrd1989@gmail.com>
2023-06-12 09:25:21 +0000

    Add moduled jlse

---
+++ config/getDuration.mjs
@@ -0,0 +1,30 @@
+import { execFile } from "prettier";
+const ffprobe = process.env.FFPROBE;
+/**
+ * 動画長取得関数
+ * @param {string} filePath ファイルパス
+ * @return number 動画長を返す (秒)
+ */
+const getDuration = (filePath) => {
+  return new Promise((resolve, reject) => {
+    execFile(
+      ffprobe,
+      ["-v", "0", "-show_format", "-of", "json", filePath],
+      (err, stdout) => {
+        if (err) {
+          reject(err);
+
+          return;
+        }
+
+        try {
+          const result = JSON.parse(stdout);
+          resolve(parseFloat(result.format.duration));
+        } catch (err) {
+          reject(err);
+        }
+      }
+    );
+  });
+};
+export { getDuration };
