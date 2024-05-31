0eb1455d5b3d46aad91573db4cbcb624d2c07f2f
Yuma Iwasaki <requrd1989@gmail.com>
2024-05-31 14:38:52 +0900

    refactor main

--- config/reGenerateThumbnail.mjs
+++ config/reGenerateThumbnail.mjs
@@ -6,28 +6,34 @@ const handle_error = (error) => {
     throw error;
 }
 
-const fetch_data = async function(url, query) {
-  try {
-      const response = await axios.get(url, { headers: { accept: "application/json" }, params: query })
-      return response.data
-  } catch (error) {
-    handle_error(error)
-  }
-}
-
-const main = async () => {
-    const record_id = process.env.RECORDEDID
-    const video_file_id = process.env.VIDEOFILEID
-    
-    if (record_id === undefined || video_file_id === undefined) {
-        throw Error(`record info is not set\nreqcord id: ${record_id}, video_file_id: ${video_file_id}`)
-    }
+const fetch_data = async function (url, query) {
     try {
-        const record = await fetch_data(`/api/recorded/${record_id}`, { "isHalfWidth": true })
-        await axios.delete(`/api/thumbnails/${record.thumbnails[0]}`)
-        await axios.post(`/api/thumbnails/videos/${video_file_id}`)
+        const response = await axios.get(url, { headers: { accept: "application/json" }, params: query })
+        return response.data
     } catch (error) {
         handle_error(error)
     }
 }
+
+const reGenerateThumbnail = async (record_id, video_file_id) => {
+    try {
+        const record = await fetch_data(`/api/recorded/${record_id}`, { "isHalfWidth": true })
+        await axios.delete(`/api/thumbnails/${record.thumbnails[0]}`)
+        await axios.post(`/api/thumbnails/videos/${video_file_id ?? record.videoFiles[0].id}`)
+    } catch (error) {
+        handle_error(error)
+    }
+}
+
+const main = async () => {
+    // 引数がセットされていた場合、優先して指定する
+    const record_id = process.argv[2] ?? process.env.RECORDEDID
+    const video_file_id = process.argv[3] ?? process.env.VIDEOFILEID
+
+    if (record_id === undefined) {
+        throw Error(`record info is not set\nreqcord id: ${record_id}`)
+    }
+    await reGenerateThumbnail(record_id, video_file_id)
+}
+
 await main()
